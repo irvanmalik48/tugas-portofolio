a878f97fc1e986ee9f2abab1541b33719a8110dd
irvanmalik48 <irvanmalik48@gmail.com>
2021-03-30 00:53:31 +0700

    assignment: new assignment
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-function navBar() {
-    var x = document.getElementById("nav");
-    if (x.className === "topnav")
-        x.className += " responsive";
-    else
-        x.className = "topnav";
-}
